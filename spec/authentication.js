320a8082312699fd603a07826937902c7f8c7027
Metalnem <metalnem@mijailovic.net>
2016-08-23 20:38:03 +0200

    Add test for authenticated message tampering

--- spec/authentication.js
+++ spec/authentication.js
@@ -12,4 +12,24 @@ describe('Authentication', () => {
 			});
 		}).catch(error => done.fail(error.message));
 	});
+
+	it('tamper with the message to verify that it will fail MAC verification', done => {
+		window.newMacKey().then(key => {
+			const message = 'The quick brown fox jumps over the lazy dog';
+			const encoded = new TextEncoder('utf-8').encode(message);
+
+			return window.generateMac(encoded, key).then(signature => {
+				encoded[encoded.length - 1] ^= 1;
+
+				return window.verifyMac(signature, encoded, key).then(valid => {
+					if (valid) {
+						done.fail('Expected message to be rejected');
+					} else {
+						expect(true).toBe(true);
+						done();
+					}
+				});
+			});
+		}).catch(error => done.fail(error.message));
+	});
 });
