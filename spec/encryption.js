d68b4f628e9ff821096f522ef730836808a0f968
Metalnem <metalnem@mijailovic.net>
2016-08-23 20:31:20 +0200

    Add test for ciphertext tampering

--- spec/encryption.js
+++ spec/encryption.js
@@ -13,4 +13,22 @@ describe('Encryption', () => {
 			});
 		}).catch(error => done.fail(error.message));
 	});
+
+	it('tamper with the ciphertext to verify that it will be rejected', done => {
+		window.newEncryptionKey().then(key => {
+			const message = 'The quick brown fox jumps over the lazy dog';
+			const encoded = new TextEncoder('utf-8').encode(message);
+
+			return window.encrypt(encoded, key).then(ciphertext => {
+				const view = new Uint8Array(ciphertext);
+				view[view.length - 1] ^= 1;
+
+				return window.decrypt(ciphertext, key)
+					.then(() => done.fail('Expected ciphertext to be rejected'));
+			});
+		}).catch(() => {
+			expect(true).toBe(true);
+			done();
+		});
+	});
 });
